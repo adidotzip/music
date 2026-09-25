dd84d865ff69024d08bf09b70b2a06da681a7b04
adi <im.adityasingh@icloud.com>
2026-09-25 17:22:53 +0000

    Use shared favorites storage for online tracks

--- src/lib/components/player/buttons/PlayerFavoriteButton.svelte
+++ src/lib/components/player/buttons/PlayerFavoriteButton.svelte
@@ -1,41 +1,10 @@
 <script lang="ts">
 	import FavoriteButton from '$lib/components/FavoriteButton.svelte'
-	import IconButton from '$lib/components/IconButton.svelte'
-	import { isFavoriteSong, toggleFavoriteSong } from '$lib/services/online-library.ts'
 
 	const player = usePlayer()
 	const track = $derived(player.activeTrack)
-	let remoteFavorite = $state(false)
-
-	$effect(() => {
-		const id = track?.remoteId
-		remoteFavorite = id ? isFavoriteSong(id) : false
-	})
-
-	const toggleRemoteFavorite = (event: MouseEvent) => {
-		if (!track?.remoteId) {
-			return
-		}
-		event.stopPropagation()
-		const next = toggleFavoriteSong(track.remoteId)
-		remoteFavorite = next
-		const target = event.currentTarget
-		if (target instanceof HTMLElement) {
-			const icon = target.querySelector('svg')
-			icon?.animate(
-				{ transform: ['scale(1)', 'scale(0.6)', 'scale(1)'] },
-				{ duration: 400, easing: 'cubic-bezier(0.4, 0, 0.2, 1)' },
-			)
-		}
-	}
 </script>
 
-{#if track?.remoteId}
-	<IconButton
-		icon={remoteFavorite ? 'favorite' : 'favoriteOutline'}
-		tooltip={remoteFavorite ? m.trackRemoveFromFavorites() : m.trackAddToFavorites()}
-		onclick={toggleRemoteFavorite}
-	/>
-{:else if track}
+{#if track}
 	<FavoriteButton trackId={track.id} favorite={track.favorite} />
 {/if}
