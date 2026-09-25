0f41ee62b75cd9f1c735abd855a19cc76aedd6d3
adi <im.adityasingh@icloud.com>
2026-09-25 14:57:42 +0000

    Use native AeroUI player controls and animations

--- src/lib/components/player/buttons/PlayNextButton.svelte
+++ src/lib/components/player/buttons/PlayNextButton.svelte
@@ -1,34 +1,30 @@
 <script lang="ts">
-	import PlayPreviousNextIcon from '../../animated-icons/PlayPreviousNextIcon.svelte'
 	import { onMount } from 'svelte'
-
 	const { class: className }: { class?: ClassValue } = $props()
 	const player = usePlayer()
 	let button: HTMLButtonElement
-
+	let skip: HTMLSpanElement
 	onMount(async () => {
-		const { initPlayerButton } = await import(
-			'https://nurislamaibekuly.github.io/aeroui/src/components/player-button/player-button.js'
-		)
+		const [{ initPlayerButton }, { initSkipLabel, playSkip }] = await Promise.all([
+			import('https://nurislamaibekuly.github.io/aeroui/src/components/player-button/player-button.js'),
+			import('https://nurislamaibekuly.github.io/aeroui/src/components/skip-label/skip-label.js')
+		])
 		initPlayerButton(button)
-		button.addEventListener('pressend', () => player.playNext())
+		initSkipLabel(skip)
+		button.addEventListener('pressend', () => {
+			playSkip(skip, { bouncing: true })
+			player.playNext()
+		})
 	})
 </script>
 
-<button
-	bind:this={button}
-	type="button"
-	class={['aero-player aero-transport-button', className]}
-	aria-label={m.playerPlayNextTrack()}
-	disabled={player.isQueueEmpty}
->
-	<PlayPreviousNextIcon type="next" />
+<button bind:this={button} type="button" class={['aero-player', className]} aria-label={m.playerPlayNextTrack()} disabled={player.isQueueEmpty}>
+	<span bind:this={skip} class="aero-skip" data-direction="forward" data-size="24" aria-hidden="true"></span>
 </button>
 
 <style lang="postcss">
 	@reference '../../../../app.css';
-
-	.aero-transport-button {
+	.aero-player {
 		--player-size: --spacing(11);
 		--player-icon: --spacing(6);
 		--player-label: var(--color-onSecondaryContainer);
