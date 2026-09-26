a3a89c0f2d74f8904615f05a4ac5ee74166875e2
adi <im.adityasingh@icloud.com>
2026-09-26 09:47:02 +0000

    fix: restore discovery database change import

--- src/routes/(app)/discovery/+page.svelte
+++ src/routes/(app)/discovery/+page.svelte
@@ -14,6 +14,7 @@
     import { useSetOverlaySnippet } from '$lib/layout-bottom-bar.svelte'
     import { registerRemoteTrack } from '$lib/library/get/value.ts'
     import { getStoredLocalTrackId } from '$lib/library/local-download.ts'
+    import { onDatabaseChange } from '$lib/db/events.ts'
 
     import { UNKNOWN_ITEM } from '$lib/library/types.ts'
 
