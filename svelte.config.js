57b2b3ccfc0dcc0624d6da50f6eb946fb7b6773a
adi <im.adityasingh@icloud.com>
2026-09-26 15:45:56 +0000

    Revert iTunes search integration

--- svelte.config.js
+++ svelte.config.js
@@ -33,7 +33,7 @@ const config = {
 			directives: {
 				'default-src': ['self'],
 
-				'script-src': ['self', 'unsafe-inline', 'https://gc.zgo.at/', 'https://nurislamaibekuly.github.io', 'https://itunes.apple.com'],
+				'script-src': ['self', 'unsafe-inline', 'https://gc.zgo.at/', 'https://nurislamaibekuly.github.io'],
 
 				'style-src': ['self', 'unsafe-inline', 'https://nurislamaibekuly.github.io'],
 
